0c5b677561c71e0d6976118830cbc13ff0376501
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2017-05-21 16:58:28 +0200

    Dashboard change password reminder

--- _bo/index.php
+++ _bo/index.php
@@ -2,6 +2,10 @@
 require_once '../vendor/autoload.php';
 
 $app = new \CrayonBo\CrayonBo();
+$settings = $app->getConfig();
+$users = $app->getManager()->getUsersList();
+$changePwd = ($users["admin"]["pwd"] === crypt("admin",$settings["salt"])) ? true : false;
 echo $app->twig->render('home.html.twig', array(
-    'menuPage' => 'home'
+    'menuPage' => 'home',
+    'changePwd' => $changePwd
 ));
