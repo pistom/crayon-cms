e10cc9a79dc3d49b290869931f82098cea9e825f
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2017-10-07 18:00:02 +0200

    Deleted old front sources

--- .frontSrc/js/getURLParameter.js
+++
@@ -1,3 +0,0 @@
-export const c = function(name) {
-    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
-}
